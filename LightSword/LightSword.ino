f129d960e156e6672c422ed60071916fa750f46b
StuckInTheCode <gala-1999@tut.by>
2018-11-11 19:58:12 +0300

    Change effect animation
    Add another effect

--- LightSword/LightSword.ino
+++ LightSword/LightSword.ino
@@ -1,14 +1,15 @@
 
 //defined parameters
-#define NUM_LEDS 30         // number of leds in the strip
-#define BTN_TIMEOUT 500     // button waiting time until pressed processing
-#define BRIGHTNESS 255      // maximum
+#define NUM_LEDS 20         // number of leds in the strip
+#define BTN_TIMEOUT 800     // button waiting time until pressed processing
+#define BRIGHTNESS 200      // maximum
 #define SWING 100           // minimum angular velocity
 #define STRONG_SWING 200    
-#define BLOW 100            // minimum acceleration
-#define HARD_BLOW 200      
+#define BLOW 45           // minimum acceleration
+#define HARD_BLOW 60      
+#define SOUND 9000
 
-#define DEBUG 0      
+#define DEBUG 1      
 #define LED_PIN 6           // led din pin
 #define BTN 3               // button pin
 #define BTN_LED 4           // control button led
@@ -44,7 +45,7 @@ unsigned long sensor_timer,swing_timer, swing_timeout;             //time of lig
 byte nowNumber;
 byte LEDcolor; 
 byte curColor, red, green, blue, redOffset, greenOffset, blueOffset;
-boolean eeprom_flag, strike_flag;
+boolean eeprom_flag, strike_flag, swing_flag, lightFlag;
 //values of coordinates get from the sensor
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
@@ -53,6 +54,10 @@ int gyrX, gyrY, gyrZ, accX, accY, accZ;
 
 
 void setup(){
+  if(DEBUG)
+  Serial.begin(9600); 
+  Wire.begin();
+  
   FastLED.addLeds<WS2811, LED_PIN, GRB>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
   FastLED.setBrightness(100);  
   pinMode(BTN, INPUT_PULLUP);
@@ -62,7 +67,8 @@ void setup(){
   digitalWrite(BTN_LED, 1);
 
   sounds.speakerPin = 9;  //setup read sounds from sd
-  sounds.setVolume(3); sounds.quality(1);
+  sounds.setVolume(5);
+  sounds.quality(1);
   if (DEBUG) 
   {
     if (SD.begin(8)) 
@@ -70,9 +76,10 @@ void setup(){
     else 
       Serial.println(F("SD fail"));
   } 
-  else 
+  else {
       SD.begin(8);
-
+  }
+  
   sensor.initialize(); //setup sensor
   sensor.setFullScaleGyroRange(MPU6050_GYRO_FS_250);
   sensor.setFullScaleAccelRange(MPU6050_ACCEL_FS_16);
@@ -84,31 +91,37 @@ void setup(){
       Serial.println(F("sensors fail"));
   }
 
-  randomSeed(analogRead(2));    // random generator
+  randomSeed(analogRead(2));              // random generator
 
   if ((EEPROM.read(0) >= 0) && (EEPROM.read(0) <= 5)) {  // if sword was activated
-    curColor = EEPROM.read(0);   // reset prev color
+    curColor = EEPROM.read(0);            // reset prev color
   } else {                       
-    EEPROM.write(0, 0);          // set 0 color
+    EEPROM.write(0, 0);                   // set 0 color
     curColor = 0;                
   }
-
+  swordState = 0;
   setColor(curColor);                  
   
   delay(1000);                        
-  setAll(0, 0, 0);                     // set black
-  FastLED.setBrightness(BRIGHTNESS);   //max brightness
+  setAll(0, 0, 0);                        // set black
+  FastLED.setBrightness(BRIGHTNESS);      //max brightness
+  nowNumber = 0;
+  sound_timer = -9000;
 }
 
 void blow() {
-  if ((ACC > BLOW) && (ACC < HARD_BLOW)) {      // если ускорение превысило порог;
-    sounds.play("Blow.wav");               // воспроизвести звук удара
-    sound_timer = millis() - 9000 + 550;
+  if ((ACC > BLOW) && (ACC < HARD_BLOW)) { // если ускорение превысило порог;
+    sounds.play("BL.wav");               // воспроизвести звук удара
+    if(DEBUG)
+        Serial.println(F("BLOW_")); 
+    sound_timer = millis() - SOUND + 560;
     strike_flag = 1;
   }
-  if (ACC >= HARD_BLOW) {           // если ускорение превысило порог
-    sounds.play("StrBlow.wav");               // воспроизвести звук удара
-    sound_timer = millis() - 9000 + 700;
+  if (ACC >= HARD_BLOW) {                    // если ускорение превысило порог
+    sounds.play("SBL.wav");               // воспроизвести звук удара
+    if(DEBUG)
+        Serial.println(F("BLOWS")); 
+    sound_timer = millis() - SOUND + 705;
     strike_flag = 1;
   }
 }
@@ -116,30 +129,32 @@ void blow() {
 void swing(){
   if (GYR > 80 && (millis() - swing_timeout > 100)) {
     swing_timeout = millis();
-    if (((millis() - swing_timer) > 100) && !strike_flag) {
-      if (GYR >= STRONG_SWING) {      // если ускорение превысило порог
-        sounds.play("Swing.wav");               // воспроизвести звук взмаха
-        sound_timer = millis() - 9000 + 350;
+    if (((millis() - swing_timer) > 500) && !strike_flag && swing_flag) {
+      if (GYR >= STRONG_SWING) {                // если ускорение превысило порог
+        sounds.play("SW.wav");               // воспроизвести звук взмаха
+        if(DEBUG)
+        Serial.println(F("SWING_")); 
+        sound_timer = millis() - SOUND + 360;
         swing_timer = millis();
+        swing_flag=0;
       }
       if ((GYR > SWING) && (GYR < STRONG_SWING)) {
-        sounds.play("StrSwing.wav");               // воспроизвести звук взмаха
-        sound_timer = millis() - 9000 + 390;
+        sounds.play("SSW.wav");              // воспроизвести звук взмаха
+        if(DEBUG)
+        Serial.println(F("SWINGS")); 
+        sound_timer = millis() - SOUND + 390;
         swing_timer = millis();
+        swing_flag=0;
       }
     }
   }
 }
 
-void loop(){
-  
+void loop(){  
   lightEffect();      
+  checkSensorState();
   on_off();             //power on/off the sword
-  if (soundEnable && ((millis() - sound_timer) >= 6000) ) {           // play main sound 6 seconds
-    sounds.play("MainSound.wav");
-    sound_timer = millis(); 
-    strike_flag = 0;                                        
-  }
+
   buttonOp();           // operation with control button
   blow();
   swing();
@@ -150,22 +165,25 @@ void on_off(){
   if (cngRequest) {                
     if (!swordState)                 //sword is on
     {                 
-        lightOn();  
-        soundEnable = 1;
+
         sounds.play("On.wav");       // play on sound
+        delay(200); 
+        lightOn();  
         if (DEBUG) Serial.println(F("ON"));                
         delay(200);  
-        swordState = true;          //change state to power on
+        soundEnable = 1;
+        swordState = 1;          //change state to power on
     } 
     else 
     {                        
-      lightOff();   
       sounds.play("Off.wav");         // play off souns
+      delay(300); 
+      lightOff();  
       soundEnable = 0;
       if (DEBUG) Serial.println(F("OFF")); 
-      sounds.disable();              
-      delay(300);                   
-      swordState = false;             
+      delay(300); 
+      sounds.disable();                                
+      swordState = 0;             
       if (eeprom_flag) 
       {                              // if color was changed
         eeprom_flag = 0;
@@ -173,6 +191,14 @@ void on_off(){
       }
     }
     cngRequest = 0;                //reset change state request
+  }
+  if (soundEnable && ((millis() - sound_timer) >= SOUND) ) {           // play main sound 6 seconds
+    //if( DEBUG)
+    //Serial.println(F("HUM"));
+    //sounds.play("SW.wav");
+    sound_timer = millis(); 
+    swing_flag = 1;
+    strike_flag = 0;                                        
   }
 }
 
@@ -185,26 +211,45 @@ void buttonOp(){
     btn_timer = millis();
   }
 
+    if (!btnState && btn_flag) {     // button was released
+    btn_flag = 0;
+    hold_flag = 0;           
+  }
+  
   // button still pressed
   if (btn_flag && btnState && (millis() - btn_timer > BTN_TIMEOUT) && !hold_flag) {
     cngRequest = 1;              // set request to change state of the sword
     hold_flag = 1;
     btn_counter = 0;
   }
-//////////////////////////////////////////////////////////////////
-    if (!btnState && btn_flag) {     // button was released
-    btn_flag = 0;
-    hold_flag = 0;           
+
+  
+ // button was pressed more than 1 time
+  if ((millis() - btn_timer > BTN_TIMEOUT) && (btn_counter != 0)) {
+    if (swordState) {
+      if (btn_counter == 3) {               
+        curColor++;                         
+        if (curColor >= 6) curColor = 0;    
+        setColor(curColor);                 
+        setAll(red, green, blue);           
+        eeprom_flag = 1;    
+        lightFlag=0;                
+      }
+      if (btn_counter == 5) {               
+        setRGB();       
+        lightFlag=1;            
+      }
+    }
+    btn_counter = 0;
   }
 }
 
 void checkSensorState(){
   if (swordState) {                                               
-    if (millis() - sensor_timer > 300) {             
+    if (millis() - sensor_timer > 600) {             
                     
       sensor.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);       // every 300ms save checked values
 
-      // найти абсолютное значение, разделить на 100
       gyrX = abs(gx / 100);
       gyrY = abs(gy / 100);
       gyrZ = abs(gz / 100);
@@ -214,7 +259,7 @@ void checkSensorState(){
 
       
       if(DEBUG){
-         Serial.print("ACC ");
+         Serial.print(F("acc "));
          Serial.print(accX);
          Serial.print(" ");
          Serial.print(accY);
@@ -222,7 +267,7 @@ void checkSensorState(){
          Serial.print(accZ);
          Serial.println(" ");
 
-         Serial.print("GYR ");
+         Serial.print("gyr ");
          Serial.print(gyrX);
          Serial.print(" ");
          Serial.print(gyrY);
@@ -232,39 +277,55 @@ void checkSensorState(){
          
     }
 
-      // найти среднеквадратичное (сумма трёх векторов в общем)
       ACC = sq((long)accX) + sq((long)accY) + sq((long)accZ);
       ACC = sqrt(ACC);
       GYR = sq((long)gyrX) + sq((long)gyrY) + sq((long)gyrZ);
       GYR = sqrt((long)GYR);
+    if(DEBUG){
+         Serial.print("ACC = ");
+         Serial.print(ACC);
+         Serial.print("GYR = ");
+         Serial.println(GYR);
+    }
 
-    sensor_timer = millis();
+    sensor_timer = micros(); 
   }
 }
 }
 
 void lightEffect(){
-  if (swordState && ((millis() - effect_timer) >= 50) ) {           // show 
-    if(nowNumber == 11)
-    nowNumber = 0;
+  if (swordState && ((millis() - effect_timer) >= 50) & !lightFlag) {           // show 
+    if(nowNumber == NUM_LEDS / 4)
+    {
+      nowNumber = 0;
+      setPixel(NUM_LEDS / 4 - 1 , red, green, blue);                              //set prev pixels to the main color
+      setPixel(NUM_LEDS / 4, red, green, blue);
+      setPixel((3*NUM_LEDS) / 4 -1, red, green, blue);
+      setPixel((3*NUM_LEDS) / 4, red, green, blue);
+    }
     
       char i0 = nowNumber;
       char i1 = NUM_LEDS / 2 - nowNumber - 1;
       char i2 = NUM_LEDS / 2 + nowNumber;
       char i3 = NUM_LEDS - nowNumber - 1;
-      
-      setPixel(i0++, red, green, blue);                              //set prev pixels to the main color
-      setPixel(i1++, red, green, blue);
-      setPixel(i2++, red, green, blue);
-      setPixel(i3++, red, green, blue);
-      
-      for (i0,i1,i2,i3; i0 < nowNumber + 4; i0++,i1--,i2++,i3--){   //set 3 next pixels to an another color  
-          setPixel(i0, red + 20, green+ 20, blue+ 20);
-          setPixel(i1, red+ 20, green+ 20, blue+ 20);
-          setPixel(i2, red+ 20, green+ 20, blue+ 20);
-          setPixel(i3, red+ 20, green+ 20, blue+ 20);
-    }
+
+      if(nowNumber){
+      setPixel(i0-1, red, green, blue);                              //set prev pixels to the main color
+      setPixel(i1+1, red, green, blue);
+      setPixel(i2-1, red, green, blue);
+      setPixel(i3+1, red, green, blue);
+      }
+      //redOffset = 
+      //greenOffset
+      //blueOffset
+      //for (i0,i1,i2,i3; i0 < nowNumber; i0++,i1--,i2++,i3--){   //set 3 next pixels to an another color  
+          setPixel(i0, abs(red - 80), abs(green- 80),abs( blue- 80));
+          setPixel(i1, abs(red - 80), abs(green- 80),abs( blue- 80));
+          setPixel(i2, abs(red - 80), abs(green- 80),abs( blue- 80));
+          setPixel(i3, abs(red - 80), abs(green- 80),abs( blue- 80));
+  //  }
     FastLED.show();
+  //  delay(100);
     nowNumber++;
   }
 }
@@ -274,6 +335,59 @@ void setPixel(int index, byte red, byte green, byte blue) {
   leds[index].r = red;
   leds[index].g = green;
   leds[index].b = blue;
+}
+
+void setRGB() {
+  char i0 = 0;
+  char i1 = NUM_LEDS / 2 - 1;
+  char i2 = NUM_LEDS / 2;
+  char i3 = NUM_LEDS - 1;
+
+  redOffset = 255;
+  greenOffset = 0;
+  blueOffset = 0;
+  
+    setPixel(i0++, redOffset, greenOffset, blueOffset);
+    setPixel(i1--, redOffset, greenOffset, blueOffset);
+    setPixel(i2++, redOffset, greenOffset, blueOffset);
+    setPixel(i3--, redOffset, greenOffset, blueOffset);
+
+  redOffset = 205;
+  greenOffset = 0;
+  blueOffset = 50;
+
+    setPixel(i0++, redOffset, greenOffset, blueOffset);
+    setPixel(i1--, redOffset, greenOffset, blueOffset);
+    setPixel(i2++, redOffset, greenOffset, blueOffset);
+    setPixel(i3--, redOffset, greenOffset, blueOffset);
+
+  redOffset = 255;
+  greenOffset = 0;
+  blueOffset = 255;
+
+    setPixel(i0++, redOffset, greenOffset, blueOffset);
+    setPixel(i1--, redOffset, greenOffset, blueOffset);
+    setPixel(i2++, redOffset, greenOffset, blueOffset);
+    setPixel(i3--, redOffset, greenOffset, blueOffset);
+
+  redOffset = 0;
+  greenOffset = 0;
+  blueOffset = 255;
+
+    setPixel(i0++, redOffset, greenOffset, blueOffset);
+    setPixel(i1--, redOffset, greenOffset, blueOffset);
+    setPixel(i2++, redOffset, greenOffset, blueOffset);
+    setPixel(i3--, redOffset, greenOffset, blueOffset);
+
+  redOffset = 100;
+  greenOffset = 255;
+  blueOffset = 0;
+
+    setPixel(i0, redOffset, greenOffset, blueOffset);
+    setPixel(i1, redOffset, greenOffset, blueOffset);
+    setPixel(i2, redOffset, greenOffset, blueOffset);
+    setPixel(i3, redOffset, greenOffset, blueOffset);
+      FastLED.show();
 }
 
 // set all pixels to the inputed color
@@ -303,6 +417,7 @@ void lightOn() {
   }
 }
 
+
 // animation of power off
 void lightOff() {
   char i0 = NUM_LEDS / 4 -1;
@@ -312,13 +427,13 @@ void lightOff() {
   //char i3 = NUM_LEDS - 1 ;
   char i3 = (NUM_LEDS * 3) / 4;
   for (i0,i1,i2,i3; i0 >=0; i0--, i1++, i2--, i3++) {         
-    setPixel(i0, red, green, blue);
-    setPixel(i1, red, green, blue);
-    setPixel(i2, red, green, blue);
-    setPixel(i3, red, green, blue);
+    setPixel(i0, 0, 0, 0);
+    setPixel(i1, 0, 0, 0);
+    setPixel(i2, 0, 0, 0);
+    setPixel(i3, 0, 0, 0);
     //setPixel((NUM_LEDS - 1 - i), red, green, blue);
     FastLED.show();
-    delay(25);
+    delay(10);
   }
 }
 
@@ -331,29 +446,29 @@ void setColor(byte c) {
       green = 0;
       blue = 0;
       break;
-    case 1:
+    case 1://pink
       red = 255;
       green = 255;
       blue = 0;
       break;
-    case 2:
+    case 2://blue
       red = 0;
       green = 255;
       blue = 0;
       break;
-    case 3:
+    case 3://green
       red = 0;
       green = 0;
       blue = 255;
       break;
 
-    case 4:
+    case 4://sky
       red = 0;
       green = 255;
       blue = 255;
       break;
       
-    case 5:
+    case 5://yellow
       red = 255;
       green = 0;
       blue = 255;
